33322e1bb59a2596ed9f0050b8b3a404a26b3c2b
Nouks <nnouka.stephen@go-groups.net>
2019-09-28 11:32:37 +0100

    Created Contact Service. Closes #6

---
+++ src/main/java/net/gogroups/trainne/contacts_api/dto/ContactDTO.java
@@ -0,0 +1,7 @@
+package net.gogroups.trainne.contacts_api.dto;
+
+import net.gogroups.trainne.contacts_api.models.Contact;
+
+public class ContactDTO extends Contact {
+
+}
---
+++ src/main/java/net/gogroups/trainne/contacts_api/services/ContactService.java
@@ -0,0 +1,7 @@
+package net.gogroups.trainne.contacts_api.services;
+
+import net.gogroups.trainne.contacts_api.dto.ContactDTO;
+
+public interface ContactService {
+    ContactDTO generate();
+}
